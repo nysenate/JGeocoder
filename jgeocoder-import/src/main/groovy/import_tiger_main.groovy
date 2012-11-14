cda5e3d72887d8460e0b8b5b469debc710bbc3a1
Graylin Kim <graylin.kim@gmail.com>
2012-11-14 14:40:50 -0500

    Now use command line args and a mysql adapter.

--- jgeocoder-import/src/main/groovy/import_tiger_main.groovy
+++ jgeocoder-import/src/main/groovy/import_tiger_main.groovy
@@ -11,59 +11,28 @@ import java.util.zip.ZipEntry
 import TigerDefinition
 import TigerTable
 import groovy.sql.Sql
-//import net.sourceforge.jgeocoder
-//import net.sourceforge.jgeocoder.us.AddressParser;
-
-//import net.sourceforge.jgeocoder.us.AddressParser;
 import org.apache.commons.lang.StringUtils
-
-//def loader = this.class.classLoader.rootLoader
-//loader.addURL(new File("C:/Users/Gateway/Documents/GitHub/JGeocoder/jgeocoder-import/jars/jgeocoder.jar").toURI().toURL())
-// can't use traditional package import
-//AliasResolver = Class.forName("net.sourceforge.jgeocoder.us.AliasResolver")
-
-
-//@Grab(group='net.sourceforge.jgeocoder', module='jgeocoder', version='1.0-SNAPSHOT')
 
 @Grab(group='commons-lang', module='commons-lang', version='2.4')
 import net.sourceforge.jgeocoder.us.AddressParser;
 import net.sourceforge.jgeocoder.AddressComponent;
 import net.sourceforge.jgeocoder.us.AddressStandardizer;
-//import net.sourceforge.jgeocoder.us.AliasResolver; //(Marking for possible delete)
 import static net.sourceforge.jgeocoder.AddressComponent.POSTDIR;
 import static net.sourceforge.jgeocoder.AddressComponent.PREDIR;
 import static net.sourceforge.jgeocoder.AddressComponent.TYPE;
 import static net.sourceforge.jgeocoder.AddressComponent.STREET;
-@GrabConfig(systemClassLoader=true)
-@Grab(group='com.h2database', module='h2', version='1.3.168')
-
 
 
 def nvl(def val, def replacement){ val==null?replacement:val}
 
-//return; //comment out to run
-
-/**
- * the raw output has total=2627282 = 441M
- * with sort | uniq will cut it down to 2588852 = 433M
- */
-
-Config config = new Config()
-//return;  //comment out to run
-
-DriverManager.registerDriver((Driver)getClass().getClassLoader().loadClass(config.driverClass).newInstance())
-println(config.driverClass)
-//this is calling a private method of DriverManager :D 
-//because the maven classloader does not have the driver class
-// connectiontype:drivername:dbpath;option1;option2;option3
-Connection conn = DriverManager.getConnection("jdbc:h2:C:\\Users\\Gateway\\Documents\\GitHub\\JGeocoder\\local\\street_data.h2;LOG=0;UNDO_LOG=0;IGNORECASE=TRUE")//, "Sa","")
-  //  new Properties(), getClass().getClassLoader())
-println(conn)
+Class.forName('com.mysql.jdbc.Driver')
+Connection conn = DriverManager.getConnection(args[1], args[2], args[3])
+conn.setAutoCommit(false);
     
 Sql sql = new Sql(conn)
 sql.execute("""DROP TABLE IF EXISTS TIGER_NY""");
 sql.execute("""
-    create table TIGER_NY ( TLID numeric not null, 
+    create table TIGER_NY ( TLID int not null, 
         ARIDL  varchar(255),
         ARIDR  varchar(255),
         LINEARID  varchar(255),
@@ -91,15 +60,14 @@ sql.execute("""
         OFFSETR  varchar(255),
         BBOX text,
         NUMPARTS text,
-        SHAPETYPE numeric, 
+        SHAPETYPE int, 
         LATLONGPAIRS text);
         """)
         
         
-
 PrintStream ps = new PrintStream(new BufferedOutputStream(new FileOutputStream(/error.csv/)))
 int total = 0, error =0;
-new File(/C:\Users\Gateway\Documents\GitHub\JGeocoder\jgeocoder-import\src\main\groovy\ConvertedData.csv/).eachLine{
+new File(args[0]).eachLine{
   if((total++)%10000==0){
     println total
     sql.commit()
@@ -109,11 +77,18 @@ new File(/C:\Users\Gateway\Documents\GitHub\JGeocoder\jgeocoder-import\src\main\
     if(StringUtils.isBlank(v) || v=='null'){ values[idx] = null}
   }
   values = new ArrayList<Object>(Arrays.asList(values))
-
+  // New to 2012 data!
+  values.remove(1);
+  values.remove(1);
+  values.remove(12);
   Map<AddressComponent, String> m
   try {
-      if (values[9]!=null){      m  = AddressParser.parseAddress("103 "+values[4]+" "+values[9])}
-      else { m  = AddressParser.parseAddress("103 "+values[4]+" "+values[10])}
+      if (values[9]!=null){
+          m  = AddressParser.parseAddress("103 "+values[4]+" "+values[9])
+      } else {
+          m  = AddressParser.parseAddress("103 "+values[4]+" "+values[10])
+      }
+
       if( m == null) {
           println("error parsing "+values)
           
@@ -130,7 +105,6 @@ new File(/C:\Users\Gateway\Documents\GitHub\JGeocoder\jgeocoder-import\src\main\
         else{FETYPE="'"+m.get(TYPE)+"'"}
     
         values.addAll(Arrays.asList(m.get(STREET),fetype,fedirs));
-        
         sql.execute("""
       insert into TIGER_NY( 
           TLID,
