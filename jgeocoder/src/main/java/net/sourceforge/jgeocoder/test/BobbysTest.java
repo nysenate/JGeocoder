7b6b92172b76bbbc95b9361318bb0f6089012304
Graylin Kim <graylin.kim@gmail.com>
2012-11-14 14:48:28 -0500

    Make mysql data source configurable.

--- jgeocoder/src/main/java/net/sourceforge/jgeocoder/test/BobbysTest.java
+++ jgeocoder/src/main/java/net/sourceforge/jgeocoder/test/BobbysTest.java
@@ -2,35 +2,46 @@ package net.sourceforge.jgeocoder.test;
 
 import java.util.concurrent.TimeUnit;
 
+import net.sourceforge.jgeocoder.CommonUtils;
+import net.sourceforge.jgeocoder.JGeocodeAddress;
+import net.sourceforge.jgeocoder.tiger.JGeocoder;
+import net.sourceforge.jgeocoder.tiger.JGeocoderConfig;
+
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
-import net.sourceforge.jgeocoder.CommonUtils;
-import net.sourceforge.jgeocoder.JGeocodeAddress;
-import net.sourceforge.jgeocoder.tiger.H2DbDataSourceFactory;
-import net.sourceforge.jgeocoder.tiger.JGeocoder;
-import net.sourceforge.jgeocoder.tiger.JGeocoderConfig;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 /**
  * @author Bobby Zheng
  *
  */
 class BobbysTest{
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ClassNotFoundException {
     JGeocoderConfig config = new JGeocoderConfig();
-    config.setJgeocoderDataHome("C:/Users/Bobby/Documents/jgeocoder/jgeocoder/src/data");
-    config.setTigerDataSource(H2DbDataSourceFactory.getH2DbDataSource("jdbc:h2:C:\\Users\\Bobby\\tiger_ny;LOG=0;UNDO_LOG=0"));
+    config.setJgeocoderDataHome("/home/graylin/projects/jgeocoder/jgeocoder/src/data");
+
+    MysqlDataSource db = new MysqlDataSource();
+    for(String arg : args) {
+        System.out.println(arg);
+    }
+    // Section dbconfig = config.get("database");
+    db.setServerName(args[0]);
+    db.setUser(args[1]);
+    db.setPassword(args[2]);
+    db.setDatabaseName(args[3]);
+
+    config.setTigerDataSource(db);
     JGeocoder jg = new JGeocoder(config);
     long start = System.currentTimeMillis();
-//    for(int i =0; i<100; i++){
+    //for(int i =0; i<100; i++){
       JGeocodeAddress addr = jg.geocodeAddress("30 Tech Valley Dr,East Greenbush, New York 12061 ");
       System.out.println(ToStringBuilder.reflectionToString(addr, ToStringStyle.MULTI_LINE_STYLE));
       JGeocodeAddress addr2 = jg.geocodeAddress("203 Hoadley Hill Rd, windsor ny 13865");
-      //JGeocodeAddress addr2 = jg.geocodeAddress("203 Hoadley Hill Rd 13865");
 
       System.out.println(ToStringBuilder.reflectionToString(addr2, ToStringStyle.MULTI_LINE_STYLE));
-//    }
+    //}
     CommonUtils.printElapsed(start, TimeUnit.SECONDS);
-    
+
     jg.cleanup();
   }
 }
